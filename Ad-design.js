ef8660dea35d007d640a1feab44599469a19a230
hablum <38866062+hablum@users.noreply.github.com>
2018-11-15 11:16:42 -0500

    Intro to JS: Ad-design

---
+++ Ad-design.js
@@ -0,0 +1,16 @@
+fill(9, 33, 130);
+
+textSize(35);
+text("Steel Sword!", 10, 30);
+
+textSize(14);
+text("Very sharp! Don't begin a duel without one!", 10, 87);
+
+fill(112, 49, 7);
+rect(272, 287, 9, 56); // Handle
+
+fill(221, 222, 218); // Thing above handle
+rect(248, 277, 59, 9);
+
+fill(158, 155, 155); // Blade
+rect(266, 155, 18, 122);
